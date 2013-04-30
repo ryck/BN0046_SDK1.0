d14ba7a77ba9ff71ef57167157d4206e8b47d7a5
Ricardo González <rickgc@gmail.com>
2013-04-30 11:25:14 +0100

    version 0.1

---
+++ src/BN0046.c
@@ -0,0 +1,352 @@
+#include "pebble_os.h"
+#include "pebble_app.h"
+#include "pebble_fonts.h"
+
+#define MY_UUID { 0xAE, 0xC3, 0x6F, 0x0D, 0x05, 0x17, 0x43, 0x9B, 0x81, 0x81, 0x05, 0x4E, 0xF5, 0x25, 0xC8, 0x65 }
+
+#define SHOW_MOON 1
+#define SHOW_DATE 1
+#define SHOW_SECONDS 1
+
+
+PBL_APP_INFO(MY_UUID,
+             "BN0046", "ryck.me",
+             0, 1, /* App version */
+             RESOURCE_ID_IMAGE_MENU_ICON,
+             APP_INFO_WATCH_FACE);
+
+Window window;
+
+Window window;
+
+Layer parent;           // Parent Layer
+BmpContainer cursor_layer;    // Seconds Layer
+
+TextLayer month; // Month Layer
+TextLayer date; // Date Layer
+TextLayer ampm; // AM/PM Layer
+TextLayer seconds; // Seconds Layer
+TextLayer moon; // Moon Layer
+
+AppTimerHandle timer_handle;
+
+#define TOTAL_IMAGE_SLOTS 4
+#define NUMBER_OF_IMAGES 10
+#define EMPTY_SLOT -1
+
+const int IMAGE_RESOURCE_IDS[NUMBER_OF_IMAGES] = {
+  RESOURCE_ID_IMAGE_NUM_0, RESOURCE_ID_IMAGE_NUM_1, RESOURCE_ID_IMAGE_NUM_2,
+  RESOURCE_ID_IMAGE_NUM_3, RESOURCE_ID_IMAGE_NUM_4, RESOURCE_ID_IMAGE_NUM_5,
+  RESOURCE_ID_IMAGE_NUM_6, RESOURCE_ID_IMAGE_NUM_7, RESOURCE_ID_IMAGE_NUM_8,
+  RESOURCE_ID_IMAGE_NUM_9
+};
+
+BmpContainer image_containers[TOTAL_IMAGE_SLOTS];
+
+int image_slot_state[TOTAL_IMAGE_SLOTS] = {EMPTY_SLOT, EMPTY_SLOT, EMPTY_SLOT, EMPTY_SLOT};
+
+char *itoa(int num)
+{
+  static char buff[20] = {};
+  int i = 0, temp_num = num, length = 0;
+  char *string = buff;
+
+  if(num >= 0) {
+    // count how many characters in the number
+    while(temp_num) {
+      temp_num /= 10;
+      length++;
+    }
+
+    // assign the number to the buffer starting at the end of the
+    // number and going to the begining since we are doing the
+    // integer to character conversion on the last number in the
+    // sequence
+    for(i = 0; i < length; i++) {
+      buff[(length-1)-i] = '0' + (num % 10);
+      num /= 10;
+    }
+    buff[i] = '\0'; // can't forget the null byte to properly end our string
+  }
+  else
+    return "Unsupported Number";
+
+  return string;
+}
+
+int get_moon_phase(int y, int m, int d) {
+  /*
+    calculates the moon phase (0-7), accurate to 1 segment.
+    0 = > new moon.
+    4 => full moon.
+    */
+
+  int c,e;
+  double jd;
+  int b;
+
+  if (m < 3) {
+      y--;
+      m += 12;
+  }
+  ++m;
+  c = 365.25*y;
+  e = 30.6*m;
+  jd = c+e+d-694039.09;  /* jd is total days elapsed */
+  jd /= 29.53;           /* divide by the moon cycle (29.53 days) */
+  b = jd;      /* int(jd) -> b, take integer part of jd */
+  jd -= b;       /* subtract integer part to leave fractional part of original jd */
+  b = jd*8 + 0.5;    /* scale fraction from 0-8 and round by adding 0.5 */
+  b = b & 7;       /* 0 and 8 are the same so turn 8 into 0 */
+  return b;
+}
+
+
+unsigned short get_display_hour(unsigned short hour) {
+
+  if (clock_is_24h_style()) {
+    return hour;
+  }
+
+  unsigned short display_hour = hour % 12;
+
+  // Converts "0" to "12"
+  return display_hour ? display_hour : 12;
+
+}
+
+void load_digit_image_into_slot(int slot_number, int digit_value) {
+
+  if ((slot_number < 0) || (slot_number >= TOTAL_IMAGE_SLOTS)) {
+    return;
+  }
+
+  if ((digit_value < 0) || (digit_value > 9)) {
+    return;
+  }
+
+  if (image_slot_state[slot_number] != EMPTY_SLOT) {
+    return;
+  }
+
+  image_slot_state[slot_number] = digit_value;
+  bmp_init_container(IMAGE_RESOURCE_IDS[digit_value], &image_containers[slot_number]);
+
+  int x;
+  int y;
+  if(slot_number == 0) {
+    x = 4;
+  y = 54;
+  }
+  if(slot_number == 1) {
+  x = 34;
+  y = 54;
+  }
+  if(slot_number == 2) {
+  x = 74;
+  y = 54;
+  }
+  if(slot_number == 3) {
+  x = 105;
+  y = 54;
+  }
+
+  image_containers[slot_number].layer.layer.frame.origin.x = x;
+  image_containers[slot_number].layer.layer.frame.origin.y = y;
+
+  layer_add_child(&window.layer, &image_containers[slot_number].layer.layer);
+
+}
+
+void unload_digit_image_from_slot(int slot_number) {
+  /*
+
+     Removes the digit from the display and unloads the image resource
+     to free up RAM.
+
+     Can handle being called on an already empty slot.
+
+   */
+
+  if (image_slot_state[slot_number] != EMPTY_SLOT) {
+    layer_remove_from_parent(&image_containers[slot_number].layer.layer);
+    bmp_deinit_container(&image_containers[slot_number]);
+    image_slot_state[slot_number] = EMPTY_SLOT;
+  }
+}
+
+void display_value(unsigned short value, unsigned short row_number, bool show_first_leading_zero) {
+  /*
+
+     Displays a numeric value between 0 and 99 on screen.
+
+     Rows are ordered on screen as:
+
+       Row 0
+       Row 1
+
+     Includes optional blanking of first leading zero,
+     i.e. displays ' 0' rather than '00'.
+
+   */
+  value = value % 100; // Maximum of two digits per row.
+
+  // Column order is: | Column 0 | Column 1 |
+  // (We process the columns in reverse order because that makes
+  // extracting the digits from the value easier.)
+  for (int column_number = 1; column_number >= 0; column_number--) {
+    int slot_number = (row_number * 2) + column_number;
+    unload_digit_image_from_slot(slot_number);
+    if (!((value == 0) && (column_number == 0) && !show_first_leading_zero)) {
+      load_digit_image_into_slot(slot_number, value % 10);
+    }
+    value = value / 10;
+  }
+}
+
+void update_display(PblTm *tick_time) {
+
+  static char am_pm_text[] = "P";
+
+  // Moon
+  #if SHOW_MOON
+    int year_number = 2012;
+    int month_number = 01;
+    int day_number = 01;
+    year_number = tick_time->tm_year + 1900;
+    month_number = tick_time->tm_mon;
+    day_number = tick_time->tm_mday;
+
+    char *temp = itoa(get_moon_phase(year_number, month_number, day_number));
+
+    text_layer_set_text(&moon, temp);
+  #endif
+
+  // Date
+  #if SHOW_DATE
+    static char month_text[] = "AAA";
+    static char date_text[] = "00";
+    string_format_time(month_text, sizeof(month_text), "%b", tick_time);
+    string_format_time(date_text, sizeof(date_text), "%e", tick_time);
+    text_layer_set_text(&month, month_text);
+    text_layer_set_text(&date, date_text);
+  #endif
+
+  // Seconds
+  #if SHOW_SECONDS
+    static char seconds_text[] = "00";
+    string_format_time(seconds_text, sizeof(seconds_text), "%S", tick_time);
+    text_layer_set_text(&seconds, seconds_text);
+  #endif
+
+  // AM/PM
+  string_format_time(am_pm_text, sizeof(am_pm_text), "%p", tick_time);
+
+  if (!clock_is_24h_style()) {
+  text_layer_set_text(&ampm, am_pm_text);
+  }
+
+  display_value(get_display_hour(tick_time->tm_hour), 0, false);
+  display_value(tick_time->tm_min, 1, true);
+}
+
+void handle_second_tick(AppContextRef ctx, PebbleTickEvent *t) {
+  (void)t;
+  (void)ctx;
+
+  update_display(t->tick_time);
+
+}
+
+void LayerSetup(PblTm *tick_time) {
+
+  //21pt
+  GFont custom_font21 = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_DIGITAL21));
+  //40pt
+  GFont custom_font45 = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_DIGITAL45));
+  //Moon
+  GFont moon_font30 = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_CLIMAICONS30));
+
+  //Init the parent layer at (0,0) and size (144 X 168)
+  layer_init(&parent, GRect(0, 0, 144, 168));
+
+  text_layer_init(&month, GRect(-25, 25, 60, 30));   // Month
+  text_layer_init(&date, GRect(48, 25, 30, 30));  // Date
+  text_layer_init(&ampm, GRect(114, 48, 30, 30));  // AM/PM
+  text_layer_init(&seconds, GRect(92, 95, 60, 60));  // Date
+  text_layer_init(&moon, GRect(105, 5, 60, 60));  // Date
+
+
+  text_layer_set_font(&month, custom_font21);
+  text_layer_set_font(&date, custom_font21);
+  text_layer_set_font(&ampm, custom_font21);
+  text_layer_set_font(&seconds, custom_font45);
+  text_layer_set_font(&moon, moon_font30);
+
+  text_layer_set_background_color(&month, GColorBlack);
+  text_layer_set_background_color(&date, GColorBlack);
+  text_layer_set_background_color(&ampm, GColorBlack);
+  text_layer_set_background_color(&seconds, GColorBlack);
+  text_layer_set_background_color(&moon, GColorBlack);
+
+  text_layer_set_text_color(&month, GColorWhite);
+  text_layer_set_text_color(&date, GColorWhite);
+  text_layer_set_text_color(&ampm, GColorWhite);
+  text_layer_set_text_color(&seconds, GColorWhite);
+  text_layer_set_text_color(&moon, GColorWhite);
+
+  text_layer_set_text_alignment(&month, GTextAlignmentRight);
+  text_layer_set_text_alignment(&date, GTextAlignmentLeft);
+  text_layer_set_text_alignment(&ampm, GTextAlignmentRight);
+  text_layer_set_text_alignment(&seconds, GTextAlignmentLeft);
+  text_layer_set_text_alignment(&moon, GTextAlignmentLeft);
+
+
+  layer_add_child(&parent, &date.layer);
+  layer_add_child(&parent, &month.layer);
+  layer_add_child(&parent, &ampm.layer);
+  layer_add_child(&parent, &seconds.layer);
+  layer_add_child(&parent, &moon.layer);
+
+  layer_add_child(&window.layer, &parent);
+
+  bmp_init_container(RESOURCE_ID_IMAGE_COLON, &cursor_layer);
+  cursor_layer.layer.layer.frame.origin.x = 64;
+  cursor_layer.layer.layer.frame.origin.y = 60;
+  layer_add_child(&parent, &cursor_layer.layer.layer);
+
+
+  update_display(tick_time);
+}
+
+void handle_init(AppContextRef ctx) {
+  (void)ctx;
+
+  window_init(&window, "BN0046 Window");
+  window_stack_push(&window, true /* Animated */);
+  window_set_background_color(&window, GColorBlack);
+
+  // If you neglect to call this, all `resource_get_handle()` requests
+  // will return NULL.
+  resource_init_current_app(&BN0046RESOURCES);
+
+  // Avoids a blank screen on watch start.
+  PblTm tick_time;
+
+  get_time(&tick_time);
+
+  LayerSetup(&tick_time);
+}
+
+void pbl_main(void *params) {
+
+  PebbleAppHandlers handlers = {
+    .init_handler = &handle_init,
+
+  .tick_info = {
+    .tick_handler = &handle_second_tick,
+    .tick_units = SECOND_UNIT
+  }
+  };
+  app_event_loop(params, &handlers);
+}
